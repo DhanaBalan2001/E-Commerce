55f12baf3315dc4af37d1268ce6d64d7ed68da1d
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-08-08 11:44:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ import {
 
 // CORS
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://crackershop.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
