2e5489667b6d318b144e06f2c81db02650fda4c1
DhanaBalan2001 <dhanabalank22@gmail.com>
2025-08-22 16:17:33 +0530

    Vite changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,13 +11,7 @@ export default defineConfig(({ mode }) => {
       outDir: 'dist',
       assetsDir: 'assets',
       sourcemap: false,
-      minify: 'terser',
-      terserOptions: {
-        compress: {
-          drop_console: mode === 'production',
-          drop_debugger: mode === 'production'
-        }
-      },
+      minify: true,
       rollupOptions: {
         output: {
           manualChunks: {
